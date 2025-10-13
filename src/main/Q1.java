de7a399dfdab4086673ed5262861d680edae548a
Ito Hisamitsu <h0779613@gmail.com>
2025-10-13 16:54:12 +0900

    Q1 #1
    done

---
+++ src/main/Q1.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+public class Q1 {
+
+	Q1(Scanner sc) {
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		String s = sc.next();
+		
+		System.out.print(a + b + c);
+		System.out.print(" ");
+		System.out.print(s);
+	}
+}
